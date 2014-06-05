e8476f30e9b5aa063d1194da13b695747f3871d3
Demotive <mat@demotive.com>
2014-06-05 15:06:09 +0100

    Carer's Allowance separated out

---
+++ server.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'sinatra'
+require 'json'
+require 'rack-cache'
+require 'net/http'
+require 'net/https'
+#require 'active_support/core_ext/hash'
+#require 'active_support/core_ext/object'
+
+use Rack::Cache
+set :public_folder, 'public'
+set :bind, '0.0.0.0'
+
+get '/' do
+  File.read(File.join('public', 'index.html'))
+end
+
+#===========
+
+# Carer's allowance live data (returns ALL claims by channel) in a nice format!
+get '/carers' do
+  cache_control :public, :max_age => 20
+  http = Net::HTTP.new('www.performance.service.gov.uk', 443)
+  http.use_ssl = true
+  req = Net::HTTP::Get.new("/data/carers-allowance/weekly-claims?collect=value%3Asum&period=month&group_by=key&duration=12")
+  response = http.request(req)
+  response.body
+end
